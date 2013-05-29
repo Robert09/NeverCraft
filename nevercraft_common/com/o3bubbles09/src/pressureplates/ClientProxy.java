85952b618845fa2ad43a39c2fe04655df7b99d1c
O3Bubbles09 <O3Bubbles09@gmail.com>
2013-05-29 00:11:28 -0700

    Deleted un-needed packages

--- nevercraft_common/com/o3bubbles09/src/pressureplates/ClientProxy.java
+++
@@ -1,5 +0,0 @@
-package com.o3bubbles09.src.pressureplates;
-
-public class ClientProxy extends CommonProxy {
-
-}
--- nevercraft_common/com/o3bubbles09/src/pressureplates/lib/ItemIds.java
+++
@@ -1,5 +0,0 @@
-package com.o3bubbles09.src.pressureplates.lib;
-
-public class ItemIds {
-
-}
